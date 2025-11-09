cfb810b9219400ab1dac454bc2da60f19d27804f
jkjorgie <jkjorgie@yahoo.com>
2025-11-09 09:52:49 -0700

    w3 la 1

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +26,12 @@ app.set("layout", "./layouts/layout");
 app.use(static);
 
 // Index route
-app.get("/", function (req, res) {
+app.get("/", baseController.buildHome);
+/*app.get("/", function (req, res) {
   res.render("index", { title: "Home" });
-});
+});*/
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
